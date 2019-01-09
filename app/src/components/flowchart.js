43395725d246093fb07f37d0988f97f837ac6a1f
arinehouse <arinehouse@yahoo.com>
2019-01-09 13:23:44 -0500

    Add react-force-graph with small example

--- app/src/components/flowchart.js
+++ app/src/components/flowchart.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
-import * as d3 from 'd3';
-import ReactFauxDOM from 'react-faux-dom'; 
+import ForceGraph2D from 'react-force-graph-2d';
+
+import { csData } from '../services/mock_data';
 
 export default class Flowchart extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            data: [4,8,15,16,23,42]
         }
-
-        this.createBarChart = this.createBarChart.bind(this);
-    }
-
-    componentDidMount() {
-        this.createBarChart();
-    }
-
-    createBarChart() {
-        let div = new ReactFauxDOM.createElement('div');
-        d3.select(div).html('hello world');
-        return div.toReact();
     }
 
     render() {
-        return this.createBarChart();
+        return (
+            <div>
+                <ForceGraph2D 
+                    graphData={csData} 
+                    nodeLabel="orc_number"
+                    linkWidth={3}
+                    linkDirectionalArrowLength={10}
+                    linkDirectionalArrowRelPos={1}
+                    backgroundColor="#e8e8e8"
+                />
+            </div>
+        );
     }
 }
