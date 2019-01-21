c668a20112bc9832fcdea5134ae4d6c7253c8a79
arinehouse <arinehouse@yahoo.com>
2019-01-21 14:24:35 -0500

    Add basic modeling

---
+++ src/models/plan.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from 'mongoose';
+
+const PlanSchema = new Schema({
+    user: { type: Schema.Types.ObjectID, ref: 'User' },
+    major1: String,
+    major2: String,
+    minor1: String,
+    minor2: String,
+    modified_main: String,
+    modified_sub: String,
+    specialization: String,
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+});
+
+// create model class
+const PlanModel = mongoose.model('Plan', PlanSchema);
+
+export default PlanModel;
