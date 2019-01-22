f32e2b2dca82cb625c043b771d7572ac68fec4c7
Raul Rodriguez <rrod.19@dartmouth.edu>
2019-01-22 09:56:49 -0500

    code

--- app/src/containers/courses.js
+++ app/src/containers/courses.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import {
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText,
+  Badge,
+} from 'reactstrap';
 import { fetchCourses } from '../actions/index';
 
 class Courses extends Component {
@@ -18,28 +25,82 @@ class Courses extends Component {
 
   renderCourses() {
     return (
-      this.props.allCourses.map((course, i) => {
+      this.props.allCourses.map((course) => {
         return (
-          <li>
-            {course.term}
-            {course.crn}
-            {course.subject}
-            {course.number}
-            {course.section}
-            {course.cross_list}
-            {course.text}
-            {course.xlist}
-            {course.period}
-            {course.room}
-            {course.building}
-            {course.instructor}
-            {course.wc}
-            {course.distrib}
-            {course.enrollment_limit}
-            {course.current_enrollment}
-            {course.status}
-            {course.learning_objective}
-          </li>
+          <ListGroupItem>
+            <ListGroupItemHeading>
+              {course.title}
+              {' '}
+              <Badge color="secondary">
+                {course.subject}
+                {course.number}
+              </Badge>
+            </ListGroupItemHeading>
+            <ListGroupItemText>
+              <ul>
+                <li>
+                  <small>Term code: </small>
+                  <small>{course.term}</small>
+                </li>
+                <li>
+                  <small>CRN: </small>
+                  <small>{course.crn}</small>
+                </li>
+                <li>
+                  <small>Section: </small>
+                  <small>{course.section}</small>
+                </li>
+                <li>
+                  <small>Xlist: </small>
+                  {course.xlist.length > 0 ? <small>{course.xlist}</small> : <small>N/A</small>}
+                </li>
+                <li>
+                  <small>WC: </small>
+                  <small>{course.wc}</small>
+                </li>
+                <li>
+                  <small>Distrib: </small>
+                  <small>{course.distrib}</small>
+                </li>
+                <li>
+                  <small>Building: </small>
+                  <small>{course.building}</small>
+                </li>
+                <li>
+                  <small>Room: </small>
+                  <small>{course.room}</small>
+                </li>
+                <li>
+                  <small>Period: </small>
+                  <small>{course.period}</small>
+                </li>
+                <li>
+                  <small>Instructor: </small>
+                  <small>{course.instructor}</small>
+                </li>
+                <li>
+                  <small>Enrollment limit: </small>
+                  <small>{course.enrollment_limit}</small>
+                </li>
+                <li>
+                  <small>Current enrollment: </small>
+                  <small>{course.current_enrollment}</small>
+                </li>
+                <li>
+                  <small>Status: </small>
+                  <small>{course.status}</small>
+                </li>
+                <li>
+                  <small>Links: </small>
+                  <Badge color="primary" href={course.description}>Description</Badge>
+                  {' '}
+                  <Badge color="primary" href={course.text}>Textbook information</Badge>
+                  {' '}
+                  {course.learning_objective.length > 0 && <Badge color="primary" href={course.learning_objective}>Learning objective</Badge>}
+                </li>
+              </ul>
+            </ListGroupItemText>
+          </ListGroupItem>
         );
       })
     );
@@ -47,9 +108,9 @@ class Courses extends Component {
 
   render() {
     return (
-      <ul>
+      <ListGroup>
         {this.renderCourses()}
-      </ul>
+      </ListGroup>
     );
   }
 }
