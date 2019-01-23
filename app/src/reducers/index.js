d0caff35510dfdcfc398bd5de13e5d18ee576414
Gillian Yue <gillianyue1999@hotmail.com>
2019-01-22 21:03:44 -0500

    connect to api

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
+import store from '../index';
+import ActionTypes from '../actions/index';
 
 import authReducer from './auth-reducer';
 import coursesReducer from './courses-reducer';
@@ -7,5 +9,10 @@ const rootReducer = combineReducers({
   auth: authReducer,
   courses: coursesReducer,
 });
+
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch({ type: ActionTypes.AUTH_USER });
+}
 
 export default rootReducer;
