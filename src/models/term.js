6c445b7de3797f31e4fab8afdeab4f2595e30c06
arinehouse <arinehouse@yahoo.com>
2019-02-04 22:30:40 -0500

    Finalize backend to store terms easily

--- src/models/term.js
+++ src/models/term.js
@@ -2,7 +2,15 @@ import mongoose, { Schema } from 'mongoose';
 
 const TermSchema = new Schema({
     plan_id: { type: Schema.Types.ObjectId, ref: 'Plan' },
-    name: String,
+    year: { // e.g. 2018, 2019. Represents the beginning of the academic year, i.e. the year of the fall term
+        type: Number,
+        min: 2000,
+        max: 2100,
+    },
+    quarter: {
+        type: String,
+        enum: ['F', 'W', 'S', 'X'],
+    },
     off_term: Boolean,
     courses: [String],
     // courses: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
@@ -13,6 +21,18 @@ const TermSchema = new Schema({
         virtuals: true,
     },
 });
+
+// define a virtual to extract the abbreviated term name
+TermSchema.virtual('name')
+    .get(function () { // eslint-disable-line prefer-arrow-callback
+        const year = this.quarter === 'F' ? (this.year % 1000).toString() : ((this.year + 1) % 1000).toString();
+        return year + this.quarter;
+    })
+    .set((abbreviated) => {
+        const [yr, quarter] = [abbreviated.slice(0, 2), abbreviated.slice(2)];
+        this.year = 2000 + yr;
+        this.quarter = quarter;
+    });
 
 // create model class
 const TermModel = mongoose.model('Term', TermSchema);
