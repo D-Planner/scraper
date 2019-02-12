03f8599e206aef20d48d52ecf5e8b2e8ce5cd755
Gillian Yue <gillianyue1999@hotmail.com>
2019-02-11 21:30:40 -0500

    remove link to dash

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -3,18 +3,14 @@ import {
   BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
 import Flowchart from './flowchart';
-<<<<<<< HEAD
-import Nav from './Nav';
-import bucket from '../containers/bucket';
-=======
 import Nav from '../containers/Nav';
->>>>>>> master
 import signUp from '../containers/signUp';
 import signIn from '../containers/signIn';
 import Courses from '../containers/courses';
 import requireAuth from '../containers/requireAuth';
 import DPlan from '../containers/dplan';
 import Plans from '../containers/plans';
+import bucket from '../containers/bucket';
 
 const FallBack = (props) => {
   return <div> URL Not Found </div>;
@@ -31,12 +27,9 @@ const App = (props) => {
           <Route exact path="/courses" component={requireAuth(Courses)} />
           <Route path="/signup" component={signUp} />
           <Route path="/signin" component={signIn} />
-<<<<<<< HEAD
           <Route path="/bucket" component={bucket} />
-=======
           <Route exact path="/plans" component={Plans} />
           <Route path="/plan/:id" component={DPlan} />
->>>>>>> master
           <Route component={FallBack} />
         </Switch>
       </div>
