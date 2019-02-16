00916d552a7b2982161cf4794bca392615ce4d36
Gillian Yue <gillianyue1999@hotmail.com>
2019-02-16 15:19:54 -0500

    ??
    now it should work

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -12,10 +12,7 @@ import requireAuth from '../containers/requireAuth';
 import DPlan from '../containers/dplan';
 import Plans from '../containers/plans';
 import bucket from '../containers/bucket';
-<<<<<<< HEAD
 import Discover from '../containers/discover';
-=======
->>>>>>> master
 
 const FallBack = (props) => {
   return <div> URL Not Found </div>;
@@ -33,11 +30,8 @@ const App = (props) => {
           <Route path="/signup" component={signUp} />
           <Route path="/signin" component={signIn} />
           <Route path="/bucket" component={bucket} />
-<<<<<<< HEAD
           <Route path="/dash" component={Dashboard} />
           <Route path="/discover" component={Discover} />
-=======
->>>>>>> master
           <Route exact path="/plans" component={Plans} />
           <Route path="/plan/:id" component={DPlan} />
           <Route component={FallBack} />
