396f72d2bcbe2ac2beb87f7074748647b2e99b2b
arinehouse <arinehouse@yahoo.com>
2019-04-28 16:19:45 -0400

    Add autofocus

--- app/src/dialogs/newPlan/index.js
+++ app/src/dialogs/newPlan/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import DialogWrapper from '../dialogWrapper';
 
 import './newPlan.scss';
@@ -6,11 +6,18 @@ import './newPlan.scss';
 const NewPlan = (props) => {
   const [text, setText] = useState('');
 
+  const inputRef = createRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
   return (
     <DialogWrapper {...props} onOk={() => { props.onOk(text); }}>
       <div className="new-plan-content">
         <div className="description">Give your new plan a name, you can change this later:</div>
         <input
+          ref={inputRef}
           type="text"
           placeholder="Untitled plan"
           className="new-plan-name"
