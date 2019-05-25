c1a05d7b2fc39fa148da60a75b9387b2c991932d
ziruihao <45774151+ziruihao@users.noreply.github.com>
2019-05-25 13:48:23 -0400

    did more styling

--- app/src/dialogs/courseInfo/index.js
+++ app/src/dialogs/courseInfo/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import DialogWrapper from '../dialogWrapper';
 
 import './courseInfo.scss';
@@ -7,7 +8,7 @@ const CourseInfoDialog = (props) => {
   console.log(props);
   return (
     <DialogWrapper {...props}>
-      {courseInfo(props.data)}
+      {courseInfo(props.data, props.nextTerm)}
     </DialogWrapper>
   );
 };
@@ -110,6 +111,10 @@ const distribTypes = [
   },
 ];
 
+/**
+ * Handles rendering of distributive bubbles.
+ * THIS FEATURE IS NOT COMPLETE, DEPENDENT ON MAKING [distrib] and [wc] BEINGS ARRAYS
+ */
 const renderDistribs = (course) => {
   const distribTypesNames = distribTypes.map(distrib => distrib.name);
   const distribs = [];
@@ -130,44 +135,123 @@ const renderDistribs = (course) => {
   }
   return (
     <div id="distribs">
-      {distribs.map((distrib) => {
-        return (
-          <img key={distrib.name} className="distrib-icon" src={distrib.icon} alt={distrib.name} />
-        );
-      })}
-      {wcs.map((wc) => {
-        return (
-          <img key={wc.name} className="wc-icon" src={wc.icon} alt={wc.name} />
-        );
-      })}
+      <div className="section-header">Distributives</div>
+      <div id="bubbles">
+        {distribs.map((distrib) => {
+          return (
+            <img key={distrib.name} className="distrib-icon" src={distrib.icon} alt={distrib.name} />
+          );
+        })}
+        {(wcs.length === 0 || distribs.length === 0) ? null : <div className="vertical-divider" />}
+        {wcs.map((wc) => {
+          return (
+            <img key={wc.name} className="wc-icon" src={wc.icon} alt={wc.name} />
+          );
+        })}
+      </div>
+
     </div>
   );
 };
 
-const courseInfo = (course) => {
+/**
+ * Handles rendering of medians, cuts off after 5 terms.
+ * @param {Array} medians
+ */
+const renderMedians = (medians) => {
+  return (
+    <div id="medians">
+      <div className="section-header">Medians</div>
+      <div id="bubbles">
+        {
+          medians.slice(0, 5).map((median) => {
+            return (
+              <div key={median.term} className="median-bubble">
+                <div className="median-bubble-grade">{median.courses[0].median}</div>
+                <div className="median-bubble-term">{median.term}</div>
+              </div>
+            );
+          })
+        }
+      </div>
+    </div>
+  );
+};
+
+/**
+ * Handles rendering of scores.
+ * @param {*} course
+ */
+const renderScores = (course) => {
+  return (
+    <div id="scores">
+      <div className="section-header">Scores</div>
+      <div>
+        Layup-list Score: {course.layuplist_score}
+      </div>
+    </div>
+  );
+};
+
+/**
+ * Handles rendering of the description.
+ * @param {String} description
+ */
+const renderDescription = (description) => {
+  return (
+    <div id="description">
+      <div className="section-header">Description</div>
+      {description}
+    </div>
+  );
+};
+
+/**
+ * Handles rendering of information for next term, if offered.
+ * THIS FEATURE IS NOT COMPLETE, DEPENDENT ON HAVING A UNIVERSAL TERM ON OUR API SERVER
+ * THIS FEATURE IS NOT COMPLETE, DEPENDENT ON FIXING THE [timeslot] PROPERTY.
+ * @param {*} course
+ * @param {String} nextTerm
+ */
+const renderNextTerm = (course, nextTerm) => {
+  if (nextTerm === course.term) {
+    return (
+      <div id="next-term">
+        <div className="section-header">Offered Next Term</div>
+        <div id="offerings">
+          <span>{course.timeslot} - hour</span>
+          <span>2A - hour</span>
+        </div>
+      </div>
+    );
+  } else return null;
+};
+
+/**
+ * Master handlers for all information about the course.
+ * @param {*} course
+ * @param {String} nextTerm
+ */
+const courseInfo = (course, nextTerm) => {
   console.log(course);
   return (
     <div id="content">
       <div id="major">Engineering Department: Prerequisite</div>
-      <hr className="divider" />
-      <div id="description">{course.description}</div>
-      <div id="current-term">
-        Latest Term:
-        {' '}
-        {course.term}
-      </div>
+      <hr className="horizontal-divider" />
+      <div id="first">{renderNextTerm(course, nextTerm)}{renderDescription(course.description)}</div>
+      <hr className="horizontal-divider" />
       <div id="metrics">
         {renderDistribs(course)}
-        <div id="medians">
-          hi
-        </div>
-        <div id="scores">
-          {course.layuplist_score}
-        </div>
+        {renderMedians(course.medians)}
+        {renderScores(course)}
       </div>
-
+      <hr className="horizontal-divider" />
     </div>
   );
 };
 
-export default CourseInfoDialog;
+const mapStateToProps = state => ({
+  nextTerm: state.time.nextTerm,
+});
+
+export default connect(mapStateToProps, null)(CourseInfoDialog);
