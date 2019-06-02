6f25c8b179d048a23f1367b2959e4edacd0d6172
arinehouse <arinehouse@yahoo.com>
2019-06-02 17:23:37 -0400

    Generate most of the api docs and add scripts to generate

--- api/src/routes/user_course.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import UserCourseController from '../controllers/user_course_controller';
-
-const userCourseRouter = Router();
-
-userCourseRouter.post('/', UserCourseController.createUserCourse);
-
-export default userCourseRouter;
