263427929e10af02e363051761944dd17dad7267
benj2468 <benjcape@gmail.com>
2019-08-01 12:21:28 +0300

    scraper fixes

--- newScraper/src/script.js
+++ newScraper/src/script.js
@@ -7,40 +7,29 @@ import departmentsJson from '../../spider/data/departments.json';
 // Helpers
 import emptyCourses from '../data/emptyCourses.json';
 import dictionary from '../data/manualEntered.json';
-import needsChecking from '../data/needsChecking.json';
+//import needsChecking from '../data/needsChecking.json';
 
 const departmentRegEx = RegExp(departmentsJson.departments.map((dep) => {
   return dep.code;
 }).join('|'));
 
-
-// Sift out Empty Courses
-// ----------------------------------------
-// const emptyCourses = courses.filter((course) => {
-//   return prerequisites[course.title].length === 0;
-// }).map((course) => {
-//   return course.title;
-// });
-// fs.writeFile('data/emtpyCourses.json', JSON.stringify(emptyCourses), (e) => {
-//   if (e) throw e;
-//   console.log('Saved');
-// });
-
 const allPrerequisites = {};
+const needsChecking = [];
 
 function save(title, prerequisites) {
   allPrerequisites[title] = prerequisites;
 }
 
+let idx = 0;
 courses
   .forEach((c, j) => {
     const prerequisites = [];
-    if (emptyCourses.includes(c.title) || needsChecking.includes(c.title)) {
+    if (emptyCourses.includes(c.title)) {
       save(c.title, prerequisites);
       return;
     }
     if (c.orc_url) { // Some don't have a url
-      // console.log(j, courses.length);
+      console.log(j + '/' + courses.length);
       const val = request('GET', c.orc_url); // Get url data
       const lastDepartment = c.department;
       try {
@@ -54,28 +43,26 @@ courses
           .each(function (i, elm) {
             if (elm.tagName === 'h3') getNext = false;
             if (getNext) {
-              preReqText += $(this).text().trim()
-                .split('Offered')[0]
-                .split('The Timetable of Class Meetings')[0]
-                .split('Please read the')[0];
+              preReqText += $(this).text().trim();
             }
             if ($(this).text().trim().includes('Prerequisite')) {
               // Get data immediatly following that which contains Prerequisite, which is always in a <h3> element
               getNext = true;
             }
           });
+        preReqText = preReqText
+                .split('Offered')[0]
+                .split('The Timetable of Class Meetings')[0]
+                .split('Please read the')[0]
+                .split('.')[0];
         if (preReqText.length > 1) {
-          // Check if we have entered this one by hand
-          if (Object.keys(dictionary).includes(preReqText)) {
-            save(c.title, dictionary[preReqText]);
-            return;
-          }
+          // Start by just getting a number, sometimes there are commas, so we don't want to neglect those
           const courseMatches = preReqText
-            .replace(/\w{3,4}(\s|-)\d{1,3}/g, (x) => {
+            .replace(/(\s|-)\d{1,3}/g, x => {
               return (`${x}@#$%^`);
             })
             .split('@#$%^')
-            .filter((x) => {
+            .filter(x => {
               return (
                 x.match(/\d/) // We need a number of some sort
                 && !x.includes('offered') // This is along with the term data below
@@ -86,9 +73,13 @@ courses
                 && !x.includes('How To Apply To')
               );
             });
+          idx += 1;
+
           let currIdx = 0;
           let currKey = '';
           let delay = false;
+          let inParen = false;
+
           courseMatches.forEach((m, i) => {
             if (m.includes('abroad')) {
               prerequisites.push({ abroad: true });
@@ -101,7 +92,6 @@ courses
               .replace('-', ' ')
               .trim()
               .toUpperCase();
-
             let tokens = trimmed.split(' ');
             if (!departmentRegEx.test(tokens[0])) {
               if (tokens.length === 1) tokens.unshift(lastDepartment);
@@ -115,11 +105,22 @@ courses
             // 'and' means that we are moving on to a new requirement
             // We only want to increment to start a new requirement if delay is false
             // We never start a new req on first iteration
+            if (m.includes('(')) {
+              inParen = true;
+              currKey = 'req';
+            }
+            if (m.includes(')')) inParen = false;
+            
+            if (inParen) {
+              if (currKey !== '' && !prerequisites[currIdx][currKey].includes(trimmed)) prerequisites[currIdx][currKey].push(trimmed);
+              return;
+            }
             if ((m.includes('one of') || m.includes('and') || m.includes('among')) && !delay && i !== 0) {
               currIdx += 1;
             }
             // This means it is time to create a new requirment object
-            if (prerequisites.length === currIdx) {
+            if (prerequisites.length === currIdx && !inParen) {
+
               // This is a range object, from one course to another
               // These needs to be checked by hand, becuase sometimes 'from' is a range, and sometimes it is 'one form' which can be either
               if ((m.includes('between') || m.includes('from')) && !m.includes('among')) {
@@ -127,6 +128,7 @@ courses
                   range: [],
                 });
                 currKey = 'range';
+                needsChecking.push(c.title);
               } else if (m.includes('grade') || m.includes('score')) {
                 prerequisites.push({
                   grade: [],
@@ -141,9 +143,7 @@ courses
             }
 
             // This makes sure we don't add the same course twice
-            if (currKey !== ''
-            && !prerequisites[currIdx][currKey].includes(trimmed)
-            ) prerequisites[currIdx][currKey].push(trimmed);
+            if (currKey !== '' && !prerequisites[currIdx][currKey].includes(trimmed)) prerequisites[currIdx][currKey].push(trimmed);
 
             // The delay
             // explanation
@@ -157,12 +157,18 @@ courses
       } catch (e) {
         console.log(e);
         console.log(`Issue Parsing: ${c.orc_url}`);
+        needsChecking.push(c.title);
       }
       save(c.title, prerequisites);
     }
   });
 
-fs.writeFile('data/prerequisites4.json', JSON.stringify(allPrerequisites), (e) => {
+fs.writeFile('data/prerequisites.json', JSON.stringify(allPrerequisites), (e) => {
+  if (e) throw e;
+  console.log('Saved');
+});
+
+fs.writeFile('data/needsChecking.json', JSON.stringify(needsChecking), (e) => {
   if (e) throw e;
   console.log('Saved');
 });
