8a8bb995d23bde41670a286c4dffb7741142b8a7
Zirui Hao <zirui.hao.22@dartmouth.edu>
2019-08-13 22:54:39 -0700

    migrated plans to within the menu bar

--- app/src/components/plans/index.js
+++ app/src/components/plans/index.js
@@ -1,48 +1,40 @@
 import React from 'react';
-import noPlan from '../../style/no-plan.png';
 import './plans.scss';
 
 /** displays information on a user's plans and allows them to select one */
 const Plans = ({
-  plans, showDialog, deletePlan, goToPlan,
+  plans, showDialog, goToPlan, active,
 }) => {
   return (
     <div className="plans">
-      <div className="plans-header">
-        <h1 className="table-header">My Plans</h1>
-        {plans.length ? renderNewPlanButton(showDialog) : <div />}
-      </div>
-      {
-    plans.length === 0
-      ? (
-        <div className="no-plans">
-          <img src={noPlan} alt="" />
-          <p>Oh no! Looks like you don’t have any plans yet. Click below to get started with your first plan.</p>
-          {renderNewPlanButton(showDialog)}
-        </div>
-      )
-      : (
-        <div className="plans-content">
-          {plans.map((plan) => {
-            return (
-              <div role="presentation" onClick={() => goToPlan(plan.id)} key={plan.id} className="plan-container">
-                <p className="plan-name">{plan.name}</p>
-                <div className="delete-button-container">
-                  <button type="button" value={plan.id} onClick={e => deletePlan(e, plan.id)}>Delete Plan</button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
+      {plans.map((plan) => {
+        return (
+          <div role="presentation" onClick={() => goToPlan(plan.id)} key={plan.id} className="plan">
+            <p className="plan-letter">{renderPlanName(plan.name, active)}</p>
+          </div>
+        );
+      })}
+      {renderNewPlanButton(showDialog)}
     </div>
   );
+};
+
+const renderPlanName = (planName, whetherActive) => {
+  if (whetherActive) {
+    if (planName.length > 20) {
+      return `${planName.substring(0, 16)}...`;
+    } else {
+      return planName;
+    }
+  } else {
+    return planName.substring(0, 1);
+  }
 };
 
 const renderNewPlanButton = (fn) => {
   return (
     <button id="newPlanButton" type="button" onClick={fn}>
-      <p>New Plan</p>
+      <p>+</p>
     </button>
   );
 };
