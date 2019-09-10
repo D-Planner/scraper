83c1da677d508b273a170b6c58e8abfb76884524
Zirui Hao <zirui.hao.22@dartmouth.edu>
2019-09-09 20:52:17 -0400

    no longer need classnames here

--- app/src/components/staticCourseElement/index.js
+++ app/src/components/staticCourseElement/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
+// import classNames from 'classnames';
 
 import '../draggableCourse/draggableCourse.scss';
 import './staticCourseElement.scss';
