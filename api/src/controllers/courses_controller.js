fb45bce83f5e343d51f71870b7e8bf542c1b3086
benj2468 <benjcape@gmail.com>
2019-09-11 21:53:55 -0400

    bug fix

--- api/src/controllers/courses_controller.js
+++ api/src/controllers/courses_controller.js
@@ -37,14 +37,13 @@ const searchCourses = (req, res) => {
     const query = Object.entries(req.query)
         .filter(([k, v]) => {
             if (k === 'department' && !departments.includes(v)) return false;
-            if (k === 'number' && v.match(/\D+/)[0] !== '.') return false;
+            if (k === 'number' && v.match(/\D+/) && v.match(/\D+/)[0] !== '.') return false;
             return v.length > 0 && k !== 'title';
         })
         .reduce((acc, [k, v]) => {
             acc[k] = v;
             return acc;
         }, {});
-    console.log(query);
     if (query.department || query.number) {
         Course.find(query)
             .populate(PopulateCourse)
