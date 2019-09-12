844097b07f85833bc2d2d96f16f7528e57b955ec
benj2468 <benjcape@gmail.com>
2019-09-12 11:56:33 -0400

    fixed times in backend, how have getter and setter for globals, scrapes from dartmouth calendar, and switches to next term in week 6

--- src/controllers/globals_controller.js
+++ src/controllers/globals_controller.js
@@ -1,20 +1,78 @@
+import cheerio from 'cheerio';
+import request from 'sync-request';
 import Globals from '../models/globals';
 
-const monthToTerm = ['W', 'S', 'X', 'F'];
 
-const setGlobals = () => {
-    const date = new Date();
-    const [year, term] = [date.getFullYear() - 2000, (date.getMonth() - 1) % 4];
-    const globals = {
-        name: 'global',
-        currentTerm: { year, term: monthToTerm[term] },
-        nextTerm: {
-            year: (term === 3) ? year + 1 : year,
-            term: monthToTerm[(term + 1) % 4],
-        },
-    };
-    Globals.findOneAndUpdate({ name: 'global' }, { globals }, { new: true });
-    return { currTerm: globals.currentTerm, nextTerm: globals.nextTerm };
+const monthToTerm = ['X', 'F', 'W', 'S'];
+const SeasonToTerm = {
+    Summer: 'X',
+    Fall: 'F',
+    Winter: 'W',
+    Spring: 'S',
 };
 
-export { setGlobals };
+const setGlobals = (req, res) => {
+    const val = request('GET', 'https://www.dartmouth.edu/~reg/calendar/academic/19-20.html');
+    const currYear = 2019;
+    const $ = cheerio.load(val.body);
+
+    const termStarts = $('div.b6 ul li')
+        .text()
+        .match(/[A-Z]\w{1,10}\s\d{1,2},\s\w{1,10}\s--\s\w{1,10}\sterm classes begin/gm)
+        .map((start) => {
+            let [date, term] = start.split(' -- ');
+
+            date = new Date(date.split(',')[0]);
+
+            Object.entries(SeasonToTerm).forEach(([k, v]) => {
+                if (term.includes(k)) term = v;
+            });
+            date.setYear(['X', 'F'].includes(term) ? currYear : currYear + 1);
+            return [date, term];
+        });
+    const week6 = termStarts.map(([date, term]) => {
+        date.setDate(date.getDate() + (6 * 7));
+        term = monthToTerm[(monthToTerm.indexOf(term) + 1) % 4];
+        return [date, term];
+    });
+    const today = new Date();
+    let term = 'X';
+    let year = currYear - 2000;
+    let i = 0;
+    while (i < week6.length) {
+        if (today > week6[i][0]) {
+            term = week6[i][1];
+            year = week6[i][0].getFullYear() - 2000;
+        }
+        i += 1;
+    }
+    const globals = {
+        name: 'global',
+        currentTerm: { year, term },
+        nextTerm: {
+            year: (term === 'F') ? year + 1 : year,
+            term: monthToTerm[(monthToTerm.indexOf(term) + 1) % 4],
+        },
+    };
+    console.log(globals);
+    Globals.findOneAndUpdate({ name: 'global' }, globals, { new: true, upsert: true }).then((g) => {
+        console.log(g);
+        res.status(200).send();
+    });
+};
+
+const getGlobals = (req, res) => {
+    Globals.findOne({ name: 'global' })
+        .then((globals) => {
+            res.status(200).send(globals);
+        }).catch((e) => {
+            console.log(e);
+            res.status(500).send({ e });
+        });
+};
+
+const GlobalsController = {
+    setGlobals,
+    getGlobals,
+};
+export default GlobalsController;
