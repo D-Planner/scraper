d5d3ced9a64fc7e783801f0dd1087a41c90be032
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2019-09-20 17:59:31 -0400

    Added user-based error message

--- app/src/containers/ErrorMessage/index.js
+++ app/src/containers/ErrorMessage/index.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import { clearError } from '../../actions';
 import GotIt from '../../../assets/buttons/got-it.png';
 
+const errorMessageText = 'We seem to have ancountered an error on our end! If this is not the first time you&#39;ve seen this error, please report it to us and we&#39;ll do our best to fix it!';
+
 class ErrorMessage extends React.Component {
   ifMessageNotNull = () => {
     if (this.props.errorMessage.message !== null) {
@@ -20,8 +22,9 @@ class ErrorMessage extends React.Component {
     return (
       <div id="error-message">
         <div id="error-box">
-          <h1>Oh no!</h1>
+          <div className="title">Oh no!</div>
           {this.ifMessageNotNull()}
+          {/* <div className="message">{errorMessageText}</div> */}
           <div id="button-container">
             <img src={GotIt} alt="Dismiss Error" onClick={this.props.clearError} />
           </div>
