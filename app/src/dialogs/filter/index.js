df372be9c8451c3f77eddba37a088205dd170489
Zirui Hao <zirui.hao.22@dartmouth.edu>
2019-09-21 09:38:53 -0400

    styled filters better

--- app/src/dialogs/filter/index.js
+++ app/src/dialogs/filter/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import DialogWrapper from '../dialogWrapper';
-import { GenEds } from '../../constants';
 
 import './filters.scss';
 
@@ -9,48 +9,64 @@ class FilterDialog extends React.Component {
     super(props);
 
     this.state = {
-      distribs: Object.values(GenEds).filter(e => (e.name !== 'W' && e.name !== 'CI' && e.name !== 'NW')).map((e) => { return (e.name: { name: e.name, checked: false }); }),
-      wcs: Object.values(GenEds).filter(e => (e.name === 'W' || e.name === 'CI' || e.name === 'NW')).map((e) => { return { name: e.name, checked: false }; }),
+      distribs: this.props.distribs,
+      wcs: this.props.wcs,
+      offeredNextTerm: this.props.offeredNextTerm,
     };
   }
 
   changeState = (name) => {
     if (name === 'W' || name === 'CI' || name === 'NW') {
+      const i = this.state.wcs.findIndex(e => e.name === name);
+      // eslint-disable-next-line react/no-access-state-in-setstate
+      const temp = Object.assign([], this.state.wcs);
+      temp[i].checked = !temp[i].checked;
+      this.setState({
+        wcs: temp,
+      });
+    } else if (name !== 'offeredNextTerm') {
+      const i = this.state.distribs.findIndex(e => e.name === name);
+      // eslint-disable-next-line react/no-access-state-in-setstate
+      const temp = Object.assign([], this.state.distribs);
+      temp[i].checked = !temp[i].checked;
+      this.setState({
+        distribs: temp,
+      });
+    } else {
       this.setState(prevState => ({
-        distribs: prevState.distribs,
+        offeredNextTerm: !prevState.offeredNextTerm,
       }));
     }
   }
 
   render() {
-    console.log(this.state.choices);
     return (
       <DialogWrapper {...this.props}>
         <div className="filter-content">
-          <div className="filter-distribs">
+          <div className="filter-distribs filter-list">
             {this.state.distribs.map((distrib) => {
               return (
-                <div className="choice" key={distrib}>
+                <div className="choice" key={distrib.name}>
                   <div className="choice-label">{distrib.name}</div>
-                  <input className="choice-input" type="checkbox" checked={distrib.checked} />
+                  <input className="choice-input" type="checkbox" checked={distrib.checked} onChange={() => this.changeState(distrib.name)} />
                 </div>
               );
             })}
           </div>
-          <div className="filter-wcs">
+          <div className="filter-wcs filter-list">
             {this.state.wcs.map((wc) => {
               return (
-                <div className="choice" key={wc}>
+                <div className="choice" key={wc.name}>
                   <div className="choice-label">{wc.name}</div>
-                  <input className="choice-input" type="checkbox" checked={wc.checked} />
+                  <input className="choice-input" type="checkbox" checked={wc.checked} onChange={() => this.changeState(wc.name)} />
                 </div>
               );
             })}
           </div>
           <div className="filter-next-term">
-            <div className="choice">
-              <div className="choice-label">Offered next term</div>
-              <input className="choice-input" type="checkbox" />
+            <div className="choice choice-next-term">
+              <div className="choice-label">Offered {this.props.currTerm.year.toString()}{this.props.currTerm.term}</div>
+              <input className="choice-input" type="checkbox" checked={this.state.offeredNextTerm} onChange={() => this.changeState('offeredNextTerm')} />
             </div>
           </div>
         </div>
@@ -59,4 +75,11 @@ class FilterDialog extends React.Component {
   }
 }
 
-export default FilterDialog;
+const mapStateToProps = state => ({
+  distribs: state.filters.distribs,
+  wcs: state.filters.wcs,
+  offeredNextTerm: state.filters.offeredNextTerm,
+  currTerm: state.time.currTerm,
+});
+
+export default connect(mapStateToProps, null)(FilterDialog);
