c9768ddf6652433692ec8040cdcae1df6289b3c2
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2019-10-31 14:04:23 -0400

    Added user email usage

--- src/controllers/verify_controller.js
+++ src/controllers/verify_controller.js
@@ -23,23 +23,49 @@ const verifyEmail = (req, res) => {
 
 const sendVerifyEmail = (req, res) => {
     User.findById(req.body.userID).then((user) => {
-        if (user.emailVerified === false && (user.verificationKey === -1 || user.verificationKey === undefined)) {
-            console.log('setting verification key');
-            setVerificationKey(req.body.userID).then((key) => {
-                console.log('key', key);
-                console.log('send verify email');
+        if (user.emailVerified === false) {
+            const sendEmailWrapper = () => {
                 generateVerificationEmail(req.body.userID).then((html) => {
-                    sendEmail('adam.j.mcquilkin.22@dartmouth.edu', 'D-Planner says hi - with a button!', html);
+                    sendEmail(user.email, 'D-Planner - Verify your email', html).then((info) => {
+                        console.log('info message');
+                        console.log(info);
+                        res.json({ info });
+                    }).catch((error) => {
+                        console.log('error message');
+                        console.error(error);
+                        res.status(500).json(error);
+                    });
                 });
-            }).catch((error) => {
-                console.error(error);
-            });
-        } else {
-            console.log('not setting verification key');
-            console.log('send verify email');
-            generateVerificationEmail(req.body.userID).then((html) => {
-                sendEmail('adam.j.mcquilkin.22@dartmouth.edu', 'D-Planner says hi - with a button!', html);
-            });
+            };
+
+            if (user.verificationKey === -1 || user.verificationKey === undefined) {
+                console.log('setting verification key');
+                setVerificationKey(req.body.userID).then((key) => {
+                    console.log('key', key);
+                    console.log('send verify email');
+                    // generateVerificationEmail(req.body.userID).then((html) => {
+                    //     sendEmail('adam.j.mcquilkin.22@dartmouth.edu', 'D-Planner says hi - with a button!', html);
+                    // });
+                    sendEmailWrapper();
+                }).catch((error) => {
+                    console.error(error);
+                });
+            } else {
+                console.log('not setting verification key');
+                console.log('send verify email');
+                // generateVerificationEmail(req.body.userID).then((html) => {
+                //     sendEmail(user.email, 'D-Planner - Verify your email', html).then((info) => {
+                //         console.log('info message');
+                //         console.log(info);
+                //         res.json({ info });
+                //     }).catch((error) => {
+                //         console.log('error message');
+                //         console.error(error);
+                //         res.status(500).json(error);
+                //     });
+                // });
+                sendEmailWrapper();
+            }
         }
     }).catch((error) => {
         console.error(error);
