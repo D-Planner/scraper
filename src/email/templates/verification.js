59177d014eab7391f7cae558e986322fda246780
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2019-10-31 22:57:54 -0400

    Added edge case verification (eg not authed)

--- src/email/templates/verification.js
+++ src/email/templates/verification.js
@@ -15,14 +15,81 @@ export function generateVerificationEmail(userID) {
             //     <tr><td><table cellspacing="0" cellpadding="0"><tr><td style="border-radius: 2px;" bgcolor="#ED2939">
             //         <a href="${frontendHost}/email/${user.verificationKey}" target="_blank" style="padding: 8px 12px; border: 1px solid #ED2939;border-radius: 2px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;">Click</a>
             //     </td></tr></table></td></tr></table></div>`);
-            resolve(`<div>
-            <div class="title">Verify your email!</div>
-            <div class="subtitle">D-Planner, the future of course election</div>
-            <p>If you did’t request a password reset, please delete this email and nothing will happen. Otherwise, click the link below to verify that you own this email address!</p>
-            <p>Verify your email here!</p>
-            <a href="${frontendHost}/email/${user.verificationKey}" target="_blank" style="padding: 8px 12px; border: 1px solid #ED2939;border-radius: 2px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;">Click</a>
-          <p>D-Planner, ©2019</p>
-        </div>`);
+            // resolve(
+            //     `<div>
+            //     <div class="title">Verify your email!</div>
+            //     <div class="subtitle">D-Planner, the future of course election</div>
+            //     <p>If you did’t request a password reset, please delete this email and nothing will happen. Otherwise, click the link below to verify that you own this email address!</p>
+            //     <p>Verify your email here!</p>
+            //     <a href="${frontendHost}/email/${user.verificationKey}" target="_blank" style="padding: 8px 12px; border: 1px solid #ED2939;border-radius: 2px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;">Click</a>
+            //     <p>D-Planner, ©2019</p>
+            //     </div>`
+            // );
+            resolve(`<html>
+              <div>
+                <div class="title">Verify your email!</div>
+                <div class="subtitle">D-Planner, the future of course election</div>
+                <p>If you didn’t request a password reset, please delete this email and nothing will happen. Otherwise, click the link below to verify that you own this email address!</p>
+                <a href="${frontendHost}/email/${user.verificationKey}" target="_blank" id="verify">Click</a>
+                <p>D-Planner, ©2019</p>
+              </div>
+            
+              <script type="text/javascript">
+                document.getElementById("verify").onclick = function () {
+                  location.href = "http://www.d-planner.com";
+                };
+              </script>
+            
+              <style>
+                @import url('https://fonts.googleapis.com/css?family=Poppins|Roboto');
+            
+                html {
+                  font-family: 'Poppins', sans-serif;
+                  width: 900px;
+                  margin: 16px auto;
+                  display: flex;
+                  flex-direction: column;
+                }
+            
+                .title {
+                  font-style: normal;
+                  font-weight: bold;
+                  font-size: 48px;
+                  color:#574966;
+                }
+            
+                .subtitle {
+                  font-style: normal;
+                  font-weight: bold;
+                  font-size: 30px;
+                  color:#574966;
+                }
+            
+                #verify {
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+            
+                  border-radius: 20px;
+                  margin: 2px;
+                  color: red;
+            
+                  width: 40%;
+                  height: 36px;
+            
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+            
+                  background: none;
+                  -webkit-text-fill-color: transparent;
+            
+                  font-weight: bold;
+                  font-size: 16px;
+                  text-transform: uppercase;
+                }
+              </style>
+            </html>`);
         }).catch((error) => {
             console.error(error);
             reject(error);
