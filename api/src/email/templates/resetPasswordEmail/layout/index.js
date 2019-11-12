4b98c1bbbfd8fb56f6cc0a5d0f3d719f6406ccd4
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2019-11-12 16:50:25 -0500

    Working email generation with React

--- api/src/email/templates/resetPasswordEmail/layout/index.js
+++ api/src/email/templates/resetPasswordEmail/layout/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
-function resetPasswordEmail({ data }) {
+function resetPasswordEmail(data) {
+    console.log('data', data);
     return (
-        <div />
+        React.createElement(
+            'div',
+            { className: 'reset-password-email-container', data },
+            // 'This is a test!',
+            // `${data}`,
+
+            // Title and subtitle
+            React.createElement('div', { className: 'title' }, 'Verify your email!'),
+            React.createElement('div', { className: 'subtitle' }, 'D-Planner, the future of course election'),
+
+            // Description
+            React.createElement('div', { className: 'body' }, 'If you didn\'t request a password reset, please delete this email and nothing will happen. Otherwise, click the link below to verify that you own this email address!'),
+
+            // Button
+            React.createElement('a', { className: 'click-link', href: data.link }, 'Verify Email!'),
+            // <a href="http://google.com" class="button">Go to Google</a>
+            // React.createElement(
+            //     'button',
+            //     {
+            //         onclick: () => { console.log('button clicked'); },
+            //     },
+            //     'test button',
+            // ),
+
+            // Copyright
+            React.createElement('div', { className: 'copyright' }, 'D-Planner, ©2019'),
+        )
     );
 }
 
--- api/src/email/templates/resetPasswordEmail/resetPasswordEmail.js
+++ api/src/email/templates/resetPasswordEmail/resetPasswordEmail.js
@@ -20,22 +20,39 @@ function getFile(relativePath) {
     });
 }
 
-function createEmail(data) {
-    return Promise.all([
-        getFile('./resetPassword.scss'),
-        getFile('./resetPasswordEmail.html'),
-    ]).then(([style, template]) => {
-        // Check imports in GitHub
-        const emailElement = React.createElement(resetPasswordEmail, { data });
-        const content = ReactDOMServer.renderToStaticMarkup(emailElement);
+function createEmail(data, htmlFile, cssFile) {
+    return new Promise((resolve, reject) => {
+        Promise.all([
+            getFile(cssFile),
+            getFile(htmlFile),
+        ]).then(([style, template]) => {
+            const emailElement = React.createElement(resetPasswordEmail, data);
+            const content = ReactDOMServer.renderToStaticMarkup(emailElement);
 
-        let emailHTML = template;
-        emailHTML = emailHTML.replace(CONTENT_TAG, content);
-        emailHTML = emailHTML.replace(STYLE_TAG, style);
+            let emailHTML = template;
+            emailHTML = emailHTML.replace(CONTENT_TAG, content);
+            emailHTML = emailHTML.replace(STYLE_TAG, style);
 
-        console.log(emailHTML);
-        return emailHTML;
+            console.log(emailHTML);
+            resolve(emailHTML);
+        });
     });
+
+    // return Promise.all([
+    //     getFile('./resetPasswordEmail.scss'),
+    //     getFile('./resetPasswordEmail.html'),
+    // ]).then(([style, template]) => {
+    //     // Check imports in GitHub
+    //     const emailElement = React.createElement(resetPasswordEmail, { data });
+    //     const content = ReactDOMServer.renderToStaticMarkup(emailElement);
+
+    //     let emailHTML = template;
+    //     emailHTML = emailHTML.replace(CONTENT_TAG, content);
+    //     emailHTML = emailHTML.replace(STYLE_TAG, style);
+
+    //     console.log(emailHTML);
+    //     return emailHTML;
+    // });
 }
 
 export default createEmail;
