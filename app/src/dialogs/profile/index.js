8ff954ffa61fbb45c93bc925a33fc229c5fe0c37
ziruihao <ziray.hao.22@dartmouth.edu>
2019-11-15 19:00:23 -0500

    fixed errors

--- app/src/dialogs/profile/index.js
+++ app/src/dialogs/profile/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import {
   removeCourseFromFavorites, removePlacement, fetchUser, fetchPlan, updateUser, fetchPlans, showDialog,
 } from '../../actions';
