39d3a9ec42383f81282e849849e145b6a49dc039
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2019-11-19 16:01:18 -0500

    Bug fix on loadingWheel

--- app/src/components/loadingWheel/index.js
+++ app/src/components/loadingWheel/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './loadingWheel.scss';
 
 const LoadingWheel = (props) => {
-  if (props.loading) {
+  if (props.loading === true) {
     return (
       <div className="loading-spinner" />
     );
