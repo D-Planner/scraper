faf81d69461c5d7adbbb6b69f4aaec41d4fb87ee
ziruihao <ziray.hao.22@dartmouth.edu>
2019-11-21 13:28:02 -0500

    quick fix with emailCheck regex

--- app/src/constants/index.js
+++ app/src/constants/index.js
@@ -415,6 +415,7 @@ const importSVGs = (r) => {
 const icons = importSVGs(require.context('../style/distrib_icons', false, /\.svg$/));
 
 // Validates email addresses
+// eslint-disable-next-line no-useless-escape
 export const emailCheckRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const GenEds = {
