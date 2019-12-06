3984c4c04c975ddc0e91c5baccde4656d7966467
benj2468 <benjcape@gmail.com>
2019-12-05 20:47:03 -0800

    ^

--- src/controllers/populators.js
+++ src/controllers/populators.js
@@ -26,6 +26,7 @@ export const PopulateTerm = [{
 
 export const PopulateUser = [{
     path: 'favorite_courses',
+    populate: PopulateCourse,
 }, {
     path: 'placement_courses',
     populate: PopulateCourse,
