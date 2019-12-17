0a1d8c6e62b4b6ea1edc3838a7415622a6c2157e
ziruihao <ziray.hao.22@dartmouth.edu>
2019-12-17 15:22:08 +0800

    refactored so the plan creation template is now in backend

---
+++ static/emptyplan.js
@@ -0,0 +1,114 @@
+// TODO autogenerate this template with a unique key and based on the user
+
+export const emptyPlan = {
+    name: 'New Plan',
+    terms: [
+        {
+            id: 1,
+            year: '2015',
+            quarter: 'F',
+            off_term: false,
+            courses: [
+            ],
+        },
+        {
+            id: 2,
+            year: '2015',
+            quarter: 'W',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 3,
+            year: '2015',
+            quarter: 'S',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 4,
+            year: '2015',
+            quarter: 'X',
+            off_term: true,
+            courses: [],
+        },
+        {
+            id: 5,
+            year: '2016',
+            quarter: 'F',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 6,
+            year: '2016',
+            quarter: 'W',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 7,
+            year: '2016',
+            quarter: 'S',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 8,
+            year: '2016',
+            quarter: 'X',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 9,
+            year: '2017',
+            quarter: 'F',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 10,
+            year: '2017',
+            quarter: 'W',
+            off_term: true,
+            courses: [],
+        },
+        {
+            id: 11,
+            year: '2017',
+            quarter: 'S',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 12,
+            year: '2017',
+            quarter: 'X',
+            off_term: true,
+            courses: [],
+        },
+        {
+            id: 13,
+            year: '2018',
+            quarter: 'F',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 14,
+            year: '2018',
+            quarter: 'W',
+            off_term: false,
+            courses: [],
+        },
+        {
+            id: 15,
+            year: '2018',
+            quarter: 'S',
+            off_term: false,
+            courses: [],
+        },
+    ],
+};
+
