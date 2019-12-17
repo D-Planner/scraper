7fcfbaa57a37f40bf9f94aae72596d4788189496
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2019-12-17 11:46:00 -0700

    Cleanup

--- src/controllers/plan_controller.js
+++ src/controllers/plan_controller.js
@@ -17,18 +17,6 @@ const getPlansByUserId = (req, res, next) => {
     });
 };
 
-// plansRouter.post('/', (req, res, next) => {
-//     PlanController.createPlanForUser(req.body.plan, req.user.id).then((newPlan) => {
-//         res.send(PlanController.sortPlan(newPlan));
-//     }).catch((err) => {
-//         if (err.name === 'MongoError' && err.code === 11000) {
-//             res.status(409).send({ err, message: 'You have already created a plan with this name' });
-//         } else {
-//             next(err);
-//         }
-//     });
-// });
-
 const createPlanForUser = async (plan, userId) => {
     try {
         // Converts strings in relevant_interests to UUIDs
