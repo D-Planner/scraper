43eaf86d780841ee3deeb2ed45d66a3550561236
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2019-12-27 17:15:06 -0500

    Added working data route

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import mongoose from 'mongoose';
 // import nodemailer from 'nodemailer';
 import sgMail from '@sendgrid/mail';
 import { requireAuth } from './authentication/init';
-import { authRouter, plansRouter, coursesRouter, termsRouter, majorsRouter, professorsRouter, globalRouter, interestsRouter, advisorRouter } from './routes';
+import { authRouter, plansRouter, coursesRouter, termsRouter, majorsRouter, professorsRouter, globalRouter, interestsRouter, advisorRouter, dataRouter } from './routes';
 import CoursesController, { trim } from './controllers/courses_controller';
 import UserModel from './models/user';
 import CourseModel from './models/course';
@@ -108,6 +108,7 @@ app.use('/professors', professorsRouter);
 app.use('/globals', requireAuth, globalRouter);
 app.use('/interests', requireAuth, interestsRouter);
 app.use('/advisors', requireAuth, advisorRouter);
+app.use('/data', requireAuth, dataRouter);
 
 // Get information for course display without being logged in
 app.get('/public/course/:id', (req, res) => {
