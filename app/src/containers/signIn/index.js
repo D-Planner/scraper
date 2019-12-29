bd8f155e88863a94efbe6c1d16ff32d78a3eac09
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2019-12-29 17:02:57 -0500

    Working signup redirect

--- app/src/containers/signIn/index.js
+++ app/src/containers/signIn/index.js
@@ -37,7 +37,10 @@ const SignInForm = withRouter(connect(null, { signinUser, validateAccessCode })(
       setErrorMessage('Please fill all required fields! (*)');
     } else {
       setErrorMessage(null);
-      props.validateAccessCode(accessCode, props.history).catch((error) => {
+      props.validateAccessCode(accessCode, props.history).then(() => {
+        console.log('resirecting to tutorial');
+        props.history.push('/tutorial/0');
+      }).catch((error) => {
         setErrorMessage(error.response.data);
       });
     }
