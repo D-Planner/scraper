90d5f2ab800a048d16ab7b60ef5dc55ecc78a2d6
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-01-10 21:30:23 -0500

    Working CSS

--- app/src/components/landing/index.js
+++ app/src/components/landing/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react';
 import './landing.scss';
-import { Link, Element } from 'react-scroll';
 import { connect } from 'react-redux';
 import Slider from 'react-slick';
 
@@ -9,15 +8,13 @@ import { showDialog } from '../../actions';
 import { DialogTypes } from '../../constants';
 import SignInForm from '../../containers/signIn';
 import SignUpForm from '../../containers/signUp';
-import Credits from '../credits';
-import pine from '../../style/pine_name.png';
-import dplanner from '../../style/dplanner-19.png';
-import dplannerTransparent from '../../style/d-planner_transparent.png';
-import arrowDown from '../../style/arrow_down.svg';
+import VideoEmbed from '../videoEmbed';
+
 import feature1 from '../../style/feature1.svg';
 import feature2 from '../../style/feature2.svg';
 import feature3 from '../../style/feature3.svg';
 import term from '../../style/term.svg';
+import downArrow from '../../style/down-arrow.svg';
 
 import asian from '../../style/asian.svg';
 import beard from '../../style/beard.svg';
@@ -29,25 +26,6 @@ import facebookIcon from '../../style/social_icons/facebook-icon-250.png';
 import twitterIcon from '../../style/social_icons/twitter-icon-250.png';
 import instagramIcon from '../../style/social_icons/instagram-icon-250.png';
 import dartmouthIcon from '../../style/social_icons/dartmouth-icon-250.png';
-
-const features = [
-  {
-    'Intelligent Course Planning': 'We\'e tired of planning our courses in Excel (or worse on paper) and we know you are too, but fear not! D-Planner is centered around an intuitive plan-based interface that allows students to plan out multiple different paths through their time at Dartmouth with different majors, minors, and more. Whether an on or off-term, D-Planner can help you feel confident in your course decisions!',
-    'Auto-Generate Plans': 'To help students who are looking for a starting point for their course plans, we offer course plan auto-generation based on user-specified off and on-terms, user-selected major and minor, courses already taken, as well as the user\'s expected graduation date. All auto-generated plans will satisfy both prerequisite hierarchy and the requirements of the major and minor, giving you the perfect starting point in finding your path!',
-  },
-  {
-    'Detailed Course Information': 'Once you\'ve found a class that looks interesting for your plan, our extensive course database offers all information publicly available on the course, from its full name and description to its distributive, past medians, LayupList score, prerequisites and classes with it as a prerequisite, and much more. You no longer need to search through multiple different sites to find all of the course information you will ever need!',
-    'Major/Minor Planning': 'There are no viable solutions for undergraduates looking to plan out their majors and minors before actually declaring sophomore year, that is until now! D-Planner offers major and minor planning features such as required classes and prerequisites. We\'ve done the hard work for you, now all that\'s left is to dream big!',
-  },
-  {
-    'Intelligent Course Search': 'Instead of needing to search for courses based on a specific and arbitrary course name (who here knows what ENGS 30 is by name?), D-Planner is equipped with a powerful and easy to use search engine that allows students to search for all of the courses in the ORC by keywords, name, term, and any other information the college maintains. Enjoy discovering your next favorite course!',
-    'Excel/Sheets Plan Import': 'We understand that students you may have created personal course plans before the advent of D-Planner, but don\'t worry! D-Planner accepts Excel and Sheets-based .csv files and allows you to import these past documents to a D-Planner plan! We have created a spreadsheet import specification that allows you to quickly and painlessly transfer your hard work to the D-Planner system!',
-  },
-  {
-    'Automatic Plan Verification': 'We\'ve listened to you, and you\'ve told us that making sure you have all of your prerequisites are fulfilled in the correct is order is hard. Thanks to our novel live prerequisite-checking algorithms, you will never again be trying to register for a class only to find out that you aren\'t eligible. Our prerequisite checking also functions with major and minor planning, allowing you to plan quickly and effectively!',
-    'Advanced Class Search': 'Besides our basic plan search, we also offer an advanced search for finding the perfect class schedule for sophomore summer (or any other term!). Advanced search includes filters ranging from broad department selection to specific distributive requirements, historical term offerings, and more! For when out basic search just isn\'t enough, advanced search is sure to kick your plan to the next level!',
-  },
-];
 
 class Landing extends React.Component {
   constructor(props) {
@@ -74,10 +52,12 @@ class Landing extends React.Component {
       slidesToScroll: 1,
       dots: true,
       infinite: true,
+      easing: 'ease-in-out',
     };
     return (
       <div className="landing-container">
         <section className="container">
+          <div style={{ display: 'flex', justifyContent: 'center' }}><img src={downArrow} alt="scroll up" className="landing-down-arrow animate" /></div>
           <div className="left">
             <div className="intro">
               <h1 className="line1">The future of Academic Planning</h1>
@@ -111,7 +91,6 @@ class Landing extends React.Component {
                 </Slider>
               </div>
             </div>
-            <img className="pine" src={pine} alt="" />
           </div>
           <div className="right">
             {this.state.signIn
@@ -137,13 +116,13 @@ class Landing extends React.Component {
               <p>That’s why we made D-Planner, a complete academic planning suite designed to turn preparing your future at college from something you dread to something you love.</p>
             </div>
           </div>
-          <div>VideoEmbed</div>
+          <VideoEmbed youtubeID="vv8EqfQMlZY" />
         </section>
         <section className="landing-section colored">
           <div className="landing-feature horizontal">
             <img src={term} alt="term" />
             <div>
-              <h3>Keyword-based Course Search</h3>
+              <h3 style={{ textAlign: 'left' }}>Keyword-based Course Search</h3>
               <p>D-Planner is equipped with a powerful and easy to use search engine. Enjoy discovering your next favorite course!</p>
             </div>
           </div>
@@ -168,7 +147,7 @@ class Landing extends React.Component {
         </section>
         <section className="landing-section">
           <h2>By students, for students</h2>
-          <div className="landing-subtitle" style={{ textAlign: 'center !important', maxWidth: '656px', margin: '0px auto 18px auto' }}>These are the faces of D-Planner.</div>
+          <div className="landing-subtitle" style={{ textAlign: 'center', margin: '0px auto 18px auto' }}>These are the faces of D-Planner.</div>
           <p style={{ textAlign: 'center', maxWidth: '656px', margin: '0px auto 58px auto' }}>Below are the faces of our D-Planner team. We know firsthand how stressful college can be, and we want to do our part to make sure that you can take of advantage of everything your experience in higher education has to offer.  Here’s a little bit about us!</p>
           <div className="landing-feature-container">
             {/* For the love of God change this text below */}
