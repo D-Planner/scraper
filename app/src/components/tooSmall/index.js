5ea22bcb13f15752163f04c4d73b2abb1d0b7f2d
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-01-15 21:39:08 -0500

    Updated tooSmall text

--- app/src/components/tooSmall/index.js
+++ app/src/components/tooSmall/index.js
@@ -12,18 +12,8 @@ const tooSmall = (props) => {
   const menuOptions = [{ name: 'Report an error', callback: () => window.open(BUG_REPORT_URL) }];
 
   return (
-  // <div id="screen-too-small">
-  //   <div className="title">Uh oh!</div>
-  //   <div className="text">
-  //     {`DPlanner is not yet available in resolutions less than ${minWidth}px by ${minHeight}px! Please use a device with a larger sceen.`}
-  //   </div>
-  //   <div className="error-disclaimer">
-  //     If you think you are seeing this message in error, please contact us at <a href={`mailto:${contactEmail}`}>{contactEmail}</a>.
-  //   </div>
-  // </div>
-
     <div style={{ height: '100vh' }}>
-      {/* <HeaderMenu menuOptions={menuOptions} /> */}
+      <HeaderMenu menuOptions={menuOptions} />
       <div className="fallback-container">
         <img className="fallback-main-feature" src={uhOhFeature} alt="404" style={{ marginBottom: '73px' }} />
         <h1 className="dark" style={{ marginBottom: '8px' }}>We can’t yet support your screen size!</h1>
