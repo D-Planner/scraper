445fb7432eaf37c41b6906e3d26e632781876290
ziruihao <ziray.hao.22@dartmouth.edu>
2020-02-10 01:11:51 -0500

    import fix

--- app/src/components/headerMenu/index.js
+++ app/src/components/headerMenu/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter, Router } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import logo from '../../style/logo.svg';
 import './headerMenu.scss';
 import { ProgressBar } from '../progressBar';
