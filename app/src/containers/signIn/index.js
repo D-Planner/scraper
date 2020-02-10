cd4a7bef447f3b396866da40e972d03a7420e499
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-02-10 10:04:41 -0500

    Added permission to all new users, removed codes from frontend

--- app/src/containers/signIn/index.js
+++ app/src/containers/signIn/index.js
@@ -52,50 +52,50 @@ const SignInForm = withRouter(connect(null, { signinUser, validateAccessCode })(
     }
   };
 
-  if (signedIn) {
-    return (
-      <div className="formContainer">
-        {props.removeTitle === true ? null : <div className="greeting">Join D-Planner today.</div>}
-        <div className="spacer">Thank you for signing up! You have been placed on the waitlist for early access, and we will notify you when we go live.</div>
-        <div className="spacer"> If you are a D-Planner advanced tester and have been given an access code, please enter it in the form below.</div>
-        <div className="spacer"> If you would like to apply to become a D-Planner advanced tester, <a href={advancedTesterFormLink}>follow this link.</a></div>
-        <div className="row" />
+  // if (signedIn) {
+  //   return (
+  //     <div className="formContainer">
+  //       {props.removeTitle === true ? null : <div className="greeting">Join D-Planner today.</div>}
+  //       <div className="spacer">Thank you for signing up! You have been placed on the waitlist for early access, and we will notify you when we go live.</div>
+  //       <div className="spacer"> If you are a D-Planner advanced tester and have been given an access code, please enter it in the form below.</div>
+  //       <div className="spacer"> If you would like to apply to become a D-Planner advanced tester, <a href={advancedTesterFormLink}>follow this link.</a></div>
+  //       <div className="row" />
+  //       <div className="row">
+  //         <input id="firstName" value={accessCode} placeholder="Access code" onKeyPress={e => (e.key === 'Enter' ? sendAccessCode() : null)} onChange={e => setAccessCode(e.target.value)} />
+  //       </div>
+  //       <ErrorMessageSpacer errorMessage={errorMessage} />
+  //       <DefaultButton click={sendAccessCode} label="Submit" />
+  //       <div className="spacer" />
+  //       {/* <button type="button" className="sign-in" onClick={props.switchToSignUp}>
+  //         <div className="button-cover"><div className="button-text">Sign Up</div></div>
+  //       </button> */}
+  //       <div role="button" tabIndex={-1} className="switch-text" onClick={props.switchToSignUp}>Need an account? Sign up</div>
+  //     </div>
+  //   );
+  // } else {
+  return (
+    <div className="formContainer">
+      <form>
+        {props.removeTitle === true ? null : <div className="greeting">Welcome back.</div>}
+        <div className="spacer">We are in pre-release, you will be able to access the D-Planner platform soon!</div>
         <div className="row">
-          <input id="firstName" value={accessCode} placeholder="Access code" onKeyPress={e => (e.key === 'Enter' ? sendAccessCode() : null)} onChange={e => setAccessCode(e.target.value)} />
+          <input id="email" type="email" value={email} placeholder="Email*" onKeyPress={e => handleKeyPress(e)} onChange={e => setEmail(e.target.value)} required />
+        </div>
+        <div className="row">
+          <input id="password" type="password" value={password} placeholder="Password*" onKeyPress={e => handleKeyPress(e)} onChange={e => setPassword(e.target.value)} required />
         </div>
         <ErrorMessageSpacer errorMessage={errorMessage} />
-        <DefaultButton click={sendAccessCode} label="Submit" />
+        <button type="button" className="sign-up" onClick={signin}>
+          <div className="button-cover"><div className="button-text">Sign In</div></div>
+        </button>
         <div className="spacer" />
-        {/* <button type="button" className="sign-in" onClick={props.switchToSignUp}>
-          <div className="button-cover"><div className="button-text">Sign Up</div></div>
-        </button> */}
+        {/* {props.showSignUp ? <DefaultButton click={props.switchToSignUp} label="Sign Up" /> : null} */}
         <div role="button" tabIndex={-1} className="switch-text" onClick={props.switchToSignUp}>Need an account? Sign up</div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="formContainer">
-        <form>
-          {props.removeTitle === true ? null : <div className="greeting">Welcome back.</div>}
-          <div className="spacer">We are in pre-release, you will be able to access the D-Planner platform soon!</div>
-          <div className="row">
-            <input id="email" type="email" value={email} placeholder="Email*" onKeyPress={e => handleKeyPress(e)} onChange={e => setEmail(e.target.value)} required />
-          </div>
-          <div className="row">
-            <input id="password" type="password" value={password} placeholder="Password*" onKeyPress={e => handleKeyPress(e)} onChange={e => setPassword(e.target.value)} required />
-          </div>
-          <ErrorMessageSpacer errorMessage={errorMessage} />
-          <button type="button" className="sign-up" onClick={signin}>
-            <div className="button-cover"><div className="button-text">Sign In</div></div>
-          </button>
-          <div className="spacer" />
-          {/* {props.showSignUp ? <DefaultButton click={props.switchToSignUp} label="Sign Up" /> : null} */}
-          <div role="button" tabIndex={-1} className="switch-text" onClick={props.switchToSignUp}>Need an account? Sign up</div>
-          <a className="switch-text" href="/reset/pass">Forgot password?</a>
-        </form>
-      </div>
-    );
-  }
+        <a className="switch-text" href="/reset/pass">Forgot password?</a>
+      </form>
+    </div>
+  );
+  // }
 }));
 
 export default SignInForm;
