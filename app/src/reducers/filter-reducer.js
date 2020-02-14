d22ad8b433a2fd3948a760fa667f142f13a6dd7a
ziruihao <ziray.hao.22@dartmouth.edu>
2020-02-13 23:10:16 -0500

    streamlined filter reducers

--- app/src/reducers/filter-reducer.js
+++ app/src/reducers/filter-reducer.js
@@ -1,7 +1,6 @@
 import { ActionTypes } from '../actions';
 import { GenEdsForDisplay as GenEds } from '../constants';
 
-
 const initialState = {
   distribs: Object.values(GenEds).filter(e => (e.name !== 'W' && e.name !== 'CI' && e.name !== 'NW')).map((e, i) => {
     return {
@@ -26,33 +25,12 @@ const initialState = {
 const filterReducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionTypes.DEAUTH_USER:
-      return {
-        distribs: Object.values(GenEds).filter(e => (e.name !== 'W' && e.name !== 'CI' && e.name !== 'NW')).map((e) => { return { name: e.name, checked: false }; }),
-        wcs: Object.values(GenEds).filter(e => (e.name === 'W' || e.name === 'CI' || e.name === 'NW')).map((e) => { return { name: e.name, checked: false }; }),
-        offered: [''].reduce((acc, cur) => {
-          const returnVal = [{ term: 'current', check: false }];
-          const terms = ['F', 'W', 'S', 'X'];
-          for (let i = 0; i < terms.length; i += 1) {
-            returnVal.push({ term: terms[i], checked: false });
-          }
-          return returnVal;
-        }, []),
-      };
+      return initialState;
     case ActionTypes.SET_FILTERS:
       return action.payload;
     case ActionTypes.CLEAR_FILTERS:
-      return {
-        distribs: Object.values(GenEds).filter(e => (e.name !== 'W' && e.name !== 'CI' && e.name !== 'NW')).map((e) => { return { name: e.name, checked: false }; }),
-        wcs: Object.values(GenEds).filter(e => (e.name === 'W' || e.name === 'CI' || e.name === 'NW')).map((e) => { return { name: e.name, checked: false }; }),
-        offered: [''].reduce((acc, cur) => {
-          const returnVal = [{ term: 'current', check: false }];
-          const terms = ['F', 'W', 'S', 'X'];
-          for (let i = 0; i < terms.length; i += 1) {
-            returnVal.push({ term: terms[i], checked: false });
-          }
-          return returnVal;
-        }, []),
-      };
+      console.log('hdiadiadjandjn');
+      return initialState;
     default:
       return state;
   }
