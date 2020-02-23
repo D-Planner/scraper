cbddf27343cb12f46f86e19ef376d3e7d9a01bb9
ziruihao <ziray.hao.22@dartmouth.edu>
2020-02-23 09:32:39 -0500

    made script promises

--- app/src/constants/requirementDecoder.js
+++ app/src/constants/requirementDecoder.js
@@ -2,8 +2,8 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable class-methods-use-this */
 /**
- * @param {requirement} A requirement object.
- * @param {plan} The user's current plan object.
+ * @param {requirement} requirement  Arequirement object.
+ * @param {plan} p The user's current plan object.
  * @returns {requirement} The same requirement object, but with fields `done` correctly set based on the user's plan.
  */
 export default class requirementDecoder {
@@ -12,41 +12,75 @@ export default class requirementDecoder {
     this.plan = plan;
   }
 
-  parseReqString(s) {
+  /**
+   * Converts the base-level requirement string like "COSC 30-50 | 60" to {department: "COSC", requirements: [{isRange: true, lowerBound: 30, upperBound: 50}, {isRange: false, number: 60}]}.
+   * @param {String} s
+   */
+  parseStringToReqObject(s) {
     s = s.split(' ');
-    const req = { isRange: false };
-    req.department = s[0];
-    if (s[1].includes('-')) {
-      req.isRange = true;
-      req.lowerBound = parseInt(s[1].split('-')[0], 10);
-      req.upperBound = parseInt(s[1].split('-')[1], 10);
-    } else {
-      req.number = s[1];
-    }
-    return req;
+    const requirement = { department: s[0] };
+    s = s.slice(1); // goes from "COSC 10-30 50-60" to "10-30 50-60"
+    s.map((subRange) => {
+      const obj = {};
+      obj.isRange = false;
+      if (subRange.includes('-')) { // we detect that it is a range "50-60" rather than just "50"
+        obj.isRange = true;
+        obj.lowerBound = parseInt(subRange.split('-')[0], 10);
+        obj.upperBound = parseInt(subRange.split('-')[1], 10);
+        return obj;
+      } else {
+        obj.number = parseInt(subRange, 10);
+        return obj;
+      }
+    });
+    requirement.requirements = s;
+    return requirement;
   }
 
-  decode() {
-    this.requirement.requirements.forEach((subReq) => {
-      if (typeof subReq.requirements === 'string') {
-        subReq = this.parseReqString(subReq.requirements);
-        this.plan.terms.forEach((t) => {
-          t.courses.forEach((c) => {
-            if (c.department === subReq.department) {
-              if (subReq.isRange) {
-                if (c.number >= subReq.lowerBound && c.number <= subReq.upperBound) {
-                  subReq.done = true;
-                  subReq.coursesUsedToFulfill.append(c.id);
+  decode(requirement) {
+    return new Promise((resolve) => {
+      // base case
+      if (typeof requirement.requirements === 'string') {
+        const outcomes = this.parseStringToReqObject(requirement.requirements);
+        outcomes.requirements.map((subReq) => {
+          this.plan.terms.forEach((t) => {
+            t.courses.forEach((c) => {
+              if (c.department === subReq.department) {
+                if (subReq.isRange) {
+                  if (c.number >= subReq.lowerBound && c.number <= subReq.upperBound) {
+                    subReq.coursesUsedToFulfill.append(c.id);
+                    return true;
+                  } else if (c.number === subReq.number) {
+                    subReq.coursesUsedToFulfill.append(c.id);
+                    return true;
+                  }
                 } else if (c.number === subReq.number) {
-                  subReq.done = true;
                   subReq.coursesUsedToFulfill.append(c.id);
+                  return true;
                 }
               }
-            }
+            });
+          });
+          return false;
+        });
+        let done = false;
+        outcomes.requirements.forEach((outcome) => {
+          if (!outcome) done = false;
+        });
+        resolve(done);
+      } else { // generic case
+        const toDo = requirement.requirements.map((subReq) => {
+          return new Promise((subResolve) => {
+            this.decode(subReq).then(outcome => subResolve(outcome));
           });
         });
-      } else {
-        decodeHelper(subReq);
+        Promise.all(toDo).then((outcomes) => {
+          let done = false;
+          outcomes.forEach((outcome) => {
+            if (!outcome) done = false;
+          });
+          resolve(done);
+        });
       }
     });
   }
