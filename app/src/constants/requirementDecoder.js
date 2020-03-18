154baefef424423eb55e0ff642a07cd371a007b3
ziruihao <ziray.hao.22@dartmouth.edu>
2020-03-18 13:35:43 -0400

    fixed parsing of major json sheets

--- app/src/constants/requirementDecoder.js
+++ app/src/constants/requirementDecoder.js
@@ -1,14 +1,17 @@
+/* eslint-disable no-useless-constructor */
 /* eslint-disable consistent-return */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable class-methods-use-this */
+// import cosc from '../../../api/static/data/majors/cosc.json';
+
 /**
- * @param {requirement} requirement  Arequirement object.
+ * @param {requirement} requirement  A requirement object.
  * @param {plan} p The user's current plan object.
  * @returns {requirement} The same requirement object, but with fields `done` correctly set based on the user's plan.
  */
-export default class requirementDecoder {
-  constructor(requirement, plan) {
-    this.requirement = requirement;
+export default class RequirementDecoder {
+  constructor(plan) {
+    // this.requirement = requirement;
     this.plan = plan;
   }
 
@@ -20,7 +23,7 @@ export default class requirementDecoder {
     s = s.split(' ');
     const requirement = { department: s[0] };
     s = s.slice(1); // goes from "COSC 10-30 50-60" to "10-30 50-60"
-    s.map((subRange) => {
+    s = s.map((subRange) => {
       const obj = {};
       obj.isRange = false;
       if (subRange.includes('-')) { // we detect that it is a range "50-60" rather than just "50"
@@ -33,6 +36,7 @@ export default class requirementDecoder {
         return obj;
       }
     });
+    requirement.done = false;
     requirement.requirements = s;
     return requirement;
   }
@@ -42,10 +46,11 @@ export default class requirementDecoder {
       // base case
       if (typeof requirement.requirements === 'string') {
         const outcomes = this.parseStringToReqObject(requirement.requirements);
-        outcomes.requirements.map((subReq) => {
+        console.log(outcomes);
+        outcomes.requirements = outcomes.requirements.map((subReq) => {
           this.plan.terms.forEach((t) => {
             t.courses.forEach((c) => {
-              if (c.department === subReq.department) {
+              if (c.department === outcomes.department) {
                 if (subReq.isRange) {
                   if (c.number >= subReq.lowerBound && c.number <= subReq.upperBound) {
                     subReq.coursesUsedToFulfill.append(c.id);
@@ -85,3 +90,176 @@ export default class requirementDecoder {
     });
   }
 }
+
+// const cosc = {
+//   name: 'Computer Science',
+//   department: 'COSC',
+//   link: 'http://dartmouth.smartcatalogiq.com/en/current/orc/Departments-Programs-Undergraduate/Computer-Science',
+//   requirements: [
+//     {
+//       name: 'Prerequisites',
+//       description: '',
+//       relationship: 'AND',
+//       requirements: [
+//         {
+//           name: 'COSC 1 or ENGS 20',
+//           description: 'ENGS 20 may substitute for COSC 1, though we recommend COSC 1 for students planning to take COSC 10.',
+//           relationship: 'OR',
+//           requirements: 'COSC 1',
+//         },
+//         {
+//           name: 'COSC 10',
+//           description: 'COSC 10',
+//           relationship: 'OR',
+//           requirements: 'COSC 10',
+//         },
+//       ],
+//     },
+//     {
+//       name: 'Core',
+//       relationship: 'AND',
+//       requirements: [
+//         {
+//           name: 'COSC 30-49 x2',
+//           description: 'Courses in theory and algorithms.',
+//           relationship: 'AND',
+//           requirements: [
+//             {
+//               name: 'COSC 30-49 [1]',
+//               description: 'First computer science course numbered 30 to 49',
+//               requirements: 'COSC 30-49',
+//             },
+//             {
+//               name: 'COSC 30-49 [2]',
+//               description: 'Second computer science course numbered 30 to 49',
+//               requirements: 'COSC 30-49',
+//             },
+//           ],
+//         },
+//         {
+//           name: 'COSC 50-69 x2',
+//           relationship: 'AND',
+//           description: 'Courses in Courses in systems and hardware.',
+//           requirements: [
+//             {
+//               name: 'COSC 50-69 [2]',
+//               description: 'First computer science course numbered 50 to 69',
+//               requirements: 'COSC 50-69',
+//             },
+//             {
+//               name: 'COSC 50-69 [2]',
+//               description: 'Second computer science course numbered 50 to 69',
+//               requirements: 'COSC 50-69',
+//             },
+//           ],
+//         },
+//         {
+//           name: 'COSC 70-89 x2',
+//           description: 'Courses in applied computer science.',
+//           relationship: 'AND',
+//           requirements: [
+//             {
+//               name: 'COSC 70-89 [1]',
+//               description: 'First computer science course numbered 70 to 89',
+//               requirements: 'COSC 70-89',
+//             },
+//             {
+//               name: 'COSC 70-89 [2]',
+//               description: 'Second computer science course numbered 70 to 89',
+//               requirements: 'COSC 70-89',
+//             },
+//           ],
+//         },
+//         {
+//           name: 'Additional courses x3',
+//           description: 'Final part of the core computer science major.',
+//           relationship: 'AND',
+//           requirements: [
+//             {
+//               name: 'COSC 30-89 x2',
+//               description: 'Electives courses in computer science.',
+//               relationship: 'AND',
+//               requirements: [
+//                 {
+//                   name: 'COSC 30-89 [1]',
+//                   description: 'First elective computer science course.',
+//                   requirements: 'COSC 30-89',
+//                 },
+//                 {
+//                   name: 'COSC 30-89 [2]',
+//                   description: 'Second elective computer science course.',
+//                   requirements: 'COSC 30-89',
+//                 },
+//               ],
+//             },
+//             {
+//               name: 'COSC 30-89 or COSC 94 or MATH 20 beyond',
+//               description: 'An elective, or COSC 94, or a math course numbered 20 or greater that is not a prerequisite to the math major and is not a seminar or a reading course.',
+//               relationship: 'OR',
+//               requirements: [
+//                 {
+//                   name: 'COSC 30-89 [3]',
+//                   description: 'Third elective computer science course.',
+//                   requirements: 'COSC 30-89',
+//                 },
+//                 {
+//                   name: 'COSC 94',
+//                   description: '',
+//                   requirements: 'COSC 94',
+//                 },
+//                 {
+//                   name: 'MATH 20 beyond',
+//                   description: 'A math course numbered 20 or greater that is not a prerequisite to the math major and is not a seminar or a reading course.',
+//                   requirements: 'MATH 20-93 95-99',
+//                 },
+//               ],
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//     {
+//       name: 'Culminating',
+//       relationship: 'OR',
+//       requirements: [
+//         {
+//           name: 'COSC 98 x2 (Project)',
+//           description: 'Two consecutive terms of COSC 98 project-based culminating experience.',
+//           relationship: 'AND',
+//           requirements: [
+//             {
+//               name: 'COSC 98',
+//               description: '',
+//               requirements: 'COSC 98',
+//             },
+//             {
+//               name: 'COSC 98',
+//               description: '',
+//               requirements: 'COSC 98',
+//             },
+//           ],
+//         },
+//         {
+//           name: 'COSC 99 x2 (Thesis)',
+//           description: 'Two consecutive terms of COSC 99 thesis-based culminating experience.',
+//           relationship: 'AND',
+//           requirements: [
+//             {
+//               name: 'COSC 99',
+//               description: '',
+//               requirements: 'COSC 99',
+//             },
+//             {
+//               name: 'COSC 99',
+//               description: '',
+//               requirements: 'COSC 99',
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//   ],
+// };
+
+// // const decoder = new RequirementDecoder();
+// // console.log(decoder.decode(cosc));
