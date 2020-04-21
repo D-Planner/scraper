271c40db0b76bacd84d7b98a10c345fe38ec81fb
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-21 14:16:03 -0400

    deleted major models

--- api/src/models/major.js
+++ api/src/models/major.js
@@ -1,42 +1,11 @@
 import mongoose, { Schema } from 'mongoose';
-import Course from './course';
-
-const RangeSchema = new Schema({
-    department: String,
-    less_than: Number,
-    greater_than: Number,
-    courses: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
-});
-
-const SetSchema = new Schema({
-    group: String,
-    options: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
-    ranges: [RangeSchema],
-});
 
 const MajorSchema = new Schema({
     name: String,
     department: String,
     link: String,
-    requirements: [SetSchema],
-}, {
-    toJSON: {
-        virtuals: true,
-    },
-});
-
-
-RangeSchema.pre('save', function (next) {
-    const range = this;
-    Course.find({
-        $and: [{ department: this.department },
-            { number: { $lt: this.less_than, $gt: this.greater_than } }],
-    }).select('_id').exec((err, doc) => {
-        if (err) { next(err); }
-        range.courses = doc;
-        range.courses.forEach((course) => { range.parent().options.push(course); });
-        next();
-    });
+    requirements: {},
+    students: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 });
 
 const MajorModel = mongoose.model('Major', MajorSchema);
